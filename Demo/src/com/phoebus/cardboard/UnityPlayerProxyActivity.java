f34938dd895ebdc9405cf4511d9af7827b0161bf
Ketaki Rao <ketaki_8@hotmail.com>
2015-04-26 11:54:03 +0530

    Source added

---
+++ Demo/src/com/phoebus/cardboard/UnityPlayerProxyActivity.java
@@ -0,0 +1,24 @@
+package com.phoebus.cardboard;
+
+import com.unity3d.player.*;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * @deprecated Use UnityPlayerActivity instead.
+ */
+public class UnityPlayerProxyActivity extends Activity
+{
+	protected void onCreate (Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+
+		Intent intent = new Intent(this, com.phoebus.cardboard.UnityPlayerActivity.class);
+		intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+		Bundle extras = getIntent().getExtras();
+		if (extras != null)
+			intent.putExtras(extras);
+		startActivity(intent);
+	}
+}
